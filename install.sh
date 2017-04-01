56304078413c7ea863bbfcd7dfa9821ab1c57150
Cosmo <hackjutsu@users.noreply.github.com>
2017-04-01 00:22:19 -0700

    Fixed the docker version

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ sudo apt-get -y install linux-image-extra-$(uname -r)
 sudo apt-get -y install apparmor
 sudo apt-get -y install apparmor
 sleep 10
-sudo apt-get -y install docker-ce=v1.13.1
+sudo apt-get -y install docker-engine=1.13.1-0~ubuntu-trusty
 sudo service docker start
